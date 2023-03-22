5db03680905b54e0f4e681c3238c13789c92d9f1
bhagyashrikubade <62027351+bhagyashrikubade@users.noreply.github.com>
2023-03-22 22:27:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_TKl4uK1pQ', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
